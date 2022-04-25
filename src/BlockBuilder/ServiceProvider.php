5c05959644034302cfdf9a086044b4f7508bafd7
alaca <ante.laca@gmail.com>
2022-04-25 08:58:21 +0200

    refactor: move blocks logic to blocks service provider

--- src/BlockBuilder/ServiceProvider.php
+++ src/BlockBuilder/ServiceProvider.php
@@ -5,8 +5,6 @@ namespace BlockFactory\BlockBuilder;
 use BlockFactory\BlockBuilder\Actions\LoadAssets;
 use BlockFactory\BlockBuilder\Actions\RegisterPostMeta;
 use BlockFactory\BlockBuilder\Actions\RegisterPostType;
-use BlockFactory\BlockBuilder\Registry\BlocksRegistry;
-use BlockFactory\BlockBuilder\Repositories\BlocksRepository;
 use BlockFactory\Framework\Helpers\Hooks;
 
 /**
@@ -18,25 +16,11 @@ class ServiceProvider extends \BlockFactory\Framework\Contracts\ServiceProvider
     /**
      * @inheritDoc
      */
-    public function register(): void
-    {
-        // Register dynamic blocks
-        foreach (BlockFactory(BlocksRepository::class)->getBlocks() as $block) {
-            BlockFactory(BlocksRegistry::class)->addBlock($block);
-        }
-    }
-
-    /**
-     * @inheritDoc
-     */
     public function boot(): void
     {
         // Post and post meta
         Hooks::addAction('init', RegisterPostType::class);
         Hooks::addAction('init', RegisterPostMeta::class);
-        // Blocks
-        Hooks::addAction('init', BlocksRegistry::class, 'registerBlocks');
-        Hooks::addFilter('block_categories_all', BlocksRegistry::class, 'registerBlocksCategories');
         // Assets
         Hooks::addAction('enqueue_block_editor_assets', LoadAssets::class);
     }
--- src/Blocks/ServiceProvider.php
+++ src/Blocks/ServiceProvider.php
@@ -3,7 +3,9 @@
 namespace BlockFactory\Blocks;
 
 use BlockFactory\BlockBuilder\Registry\BlocksRegistry;
+use BlockFactory\BlockBuilder\Repositories\BlocksRepository;
 use BlockFactory\Blocks\Container\ContainerBlock;
+use BlockFactory\Framework\Helpers\Hooks;
 
 /**
  * Service Provider responsible for loading Block Factory blocks
@@ -21,5 +23,19 @@ class ServiceProvider extends \BlockFactory\Framework\Contracts\ServiceProvider
         BlockFactory(BlocksRegistry::class)->addBlocks([
             ContainerBlock::class
         ]);
+
+        // Dynamic blocks
+        foreach (BlockFactory(BlocksRepository::class)->getBlocks() as $block) {
+            BlockFactory(BlocksRegistry::class)->addBlock($block);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function boot(): void
+    {
+        Hooks::addAction('init', BlocksRegistry::class, 'registerBlocks');
+        Hooks::addFilter('block_categories_all', BlocksRegistry::class, 'registerBlocksCategories');
     }
 }
